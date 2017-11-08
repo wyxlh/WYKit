f64da572741b64bb07c721c0f30671188505bf4a
wyxlh <wangyu@wangyudeMacBook-Pro.local>
2017-11-08 14:35:40 +0800

    添加 tableView 动画

---
+++ WKFrameDemo/WKFrameDemo/UserCenter/Controller/CenterChildren/WKTableAnimationsListViewController.h
@@ -0,0 +1,13 @@
+//
+//  WKTableAnimationsListViewController.h
+//  WKFrameDemo
+//
+//  Created by 王宇 on 2017/10/18.
+//  Copyright © 2017年 wy. All rights reserved.
+//
+
+#import "WKBaseViewController.h"
+
+@interface WKTableAnimationsListViewController : WKBaseViewController
+
+@end
---
+++ WKFrameDemo/WKFrameDemo/UserCenter/Controller/CenterChildren/WKTableViewAnDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  WKTableViewAnDetailViewController.h
+//  WKFrameDemo
+//
+//  Created by 王宇 on 2017/10/18.
+//  Copyright © 2017年 wy. All rights reserved.
+//
+
+#import "WKBaseViewController.h"
+
+@interface WKTableViewAnDetailViewController : WKBaseViewController
+
+@end
